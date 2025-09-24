c4645ed82a5f51581ab6a4754f78e691d7104de4
trick4u <thinkbrain10@gmail.com>
2025-09-24 16:56:58 +0530

    controller_upgrade

---
+++ lib/controllers/trade_controller.dart
@@ -0,0 +1,7 @@
+
+
+import 'package:get/get.dart';
+
+class TradeController extends GetxController {
+  // TradeController implementation
+}
--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../controllers/dashboard_controller.dart';
+import '../controllers/import_trade_controller.dart';
 import '../controllers/navigation_sontroller.dart';
+import '../controllers/profile_controller.dart';
+import '../controllers/reports_controller.dart';
+import '../controllers/trade_controller.dart';
+import '../main_api_client.dart';
 import 'dashboard_page.dart';
 import 'trade_page.dart';
 import 'profile_page.dart';
@@ -9,7 +15,17 @@ import 'import_trade_page.dart';
 import 'reports_page.dart';
 
 class MainScreen extends StatelessWidget {
-  final NavigationController navController = Get.put(NavigationController());
+  final NavigationController navController = Get.put(NavigationController(), permanent: true);
+
+
+  // Initialize other controllers
+  final DashBoardController dashBoardController = Get.put(DashBoardController(
+      mainApiClient: Get.find<MainApiClient>()
+  ), permanent: true);
+  final TradeController tradeController = Get.put(TradeController(), permanent: true);
+  final ImportTradeController importTradeController = Get.put(ImportTradeController(), permanent: true);
+  final ReportsController reportsController = Get.put(ReportsController(), permanent: true);
+  final ProfileController profileController = Get.put(ProfileController(), permanent: true);
 
   final List<Widget> pages = [
     DashboardPage(),
